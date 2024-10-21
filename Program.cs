c5db97c14627261880d49ce771ed8f3663739a99
Douglas Nunes <paulodnunes2016@gmail.com>
2024-10-20 21:32:11 -0300

    Imprimiendo os tracos do tabuleiro

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace Xadrez10
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8,8);
+            Tela.ImprimirTabuleiro(tab);
+
 
             Console.ReadLine();
 
