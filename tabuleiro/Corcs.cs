fe34159fa0c3c948133700dd3cb00cc0dd835a9d
Douglas Nunes <paulodnunes2016@gmail.com>
2024-10-20 21:01:56 -0300

    Implementando a funcao do tabuleiro

---
+++ tabuleiro/Corcs.cs
@@ -0,0 +1,13 @@
+﻿namespace tabuleiro
+{
+    enum Corcs
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+    }
+}
---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿namespace tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Corcs cor { get;protected set; }
+        public int QtdeMovementos { get;protected set; }
+        public Tabuleiro Tab { get;protected set; }
+
+        public Peca(Posicao posicao, Corcs cor, Tabuleiro tab)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            Tab = tab;
+            QtdeMovementos = 0;
+
+        }
+    }
+}
